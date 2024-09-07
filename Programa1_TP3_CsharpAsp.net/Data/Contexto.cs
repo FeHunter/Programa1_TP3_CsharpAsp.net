c6993909e8451ea174dafa745ec03d8ee85ea834
FeHunter <hunterfe1111@gmail.com>
2024-09-07 15:10:34 -0300

    Adicionar arquivos de projeto.

---
+++ Programa1_TP3_CsharpAsp.net/Data/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Programa1_TP3_CsharpAsp.net.Models;
+
+namespace Programa1_TP3_CsharpAsp.net.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base (options) { }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ Programa1_TP3_CsharpAsp.net/Models/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace Programa1_TP3_CsharpAsp.net.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
